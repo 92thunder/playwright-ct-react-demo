5135ee4436bde745c7a04d3740bde618f0d3369f
ryota.kunisada <r.kunisada661@gmail.com>
2024-07-23 18:09:00 +0900

    add count up test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -3,7 +3,13 @@ import App from "./App";
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test("should work", async ({ mount }) => {
+test("should work", async ({ mount, page }) => {
   const component = await mount(<App />);
   await expect(component).toContainText("Vite");
+
+  const countUpButton = await page.getByRole("button", {
+    name: "count up button",
+  });
+  await countUpButton.click();
+  expect(countUpButton).toHaveText("count is 1");
 });
